77de41d1cdee6d80f7470a379d0943a2c00d7062
chriswininger <chris.wininger@gmail.com>
2015-07-24 13:59:58 -0400

    corrected config file

--- docker-source/etc/simplesamlphp/config.php
+++ docker-source/etc/simplesamlphp/config.php
@@ -22,7 +22,7 @@ $config = array(
      * reverse proxy).
      */
     'baseurlpath' => 'simplesamlphp/',
-    'certdir' => '/etc/ssl/certs/',
+    'certdir' => '/etc/certs/ssl/',
     'loggingdir' => '/var/log/simplesamlphp/',
     'datadir' => '/var/lib/simplesamlphp/data/',
     'metadatadir' => '/etc/simplesamlphp/metadata/',
